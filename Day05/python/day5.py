724d8900a6b8d46bba30190a17e8c999fd4af1a9
David Lachut <david.lachut@jhuapl.edu>
2020-12-08 11:28:44 -0500

    improved through cheating off of JR

--- Day05/python/day5.py
+++ Day05/python/day5.py
@@ -2,19 +2,15 @@ import sys
 
 with open('../input.txt') as FILE:
     tickets = [
-        l.strip()\
-        .replace('F', '0')\
-        .replace('B', '1')\
-        .replace('R', '1')\
+        l.strip()
+        .replace('F', '0')
+        .replace('B', '1')
+        .replace('R', '1')
         .replace('L', '0') 
         for l in FILE.readlines()
     ]
     tickets = [
-        [int(t[:-3], base=2), int(t[-3:], base=2)]
-        for t in tickets
-    ]
-    tickets = [
-        [t[0], t[1], t[0] * 8 + t[1]]
+        [int(t[:-3], base=2), int(t[-3:], base=2), int(t, base=2)]
         for t in tickets
     ]
 
@@ -22,10 +18,8 @@ stix = sorted(tickets, key=lambda x:x[2])
 
 if sys.argv[1].lower() == "first":
     print(stix[-1][-1])
-    sys.exit(0)
-if sys.argv[1].lower() != "second":
-    sys.exit(1)
-
-ids = list(zip(*stix))[2]
-
-print(max(set(range(stix[-1][-1] + 1)).difference(ids)))
+elif sys.argv[1].lower() == "second":
+    ids = list(zip(*stix))[2]
+    print(max(set(range(stix[-1][-1] + 1)).difference(ids)))
+else:
+    sys.exit(-1)
